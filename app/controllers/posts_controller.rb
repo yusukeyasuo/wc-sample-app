a291cd10542ed0569875296164594a415eed94ad
Yusuke Yasuo <yasuo.y@dely.jp>
2019-06-15 04:11:33 +0000

    7章

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,17 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
 
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
+  end
+  
   def new
     @post = Post.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'posts' => 'posts#index'
   get 'posts/new'
   get 'posts/:id' => 'posts#show', as: 'post'
-  get 'top' => 'root#top'
-  
+  get 'posts/:id/edit' => 'posts#edit', as: 'edit_post'
+  patch 'posts/:id' => 'posts#update', as: 'update_post'
   post 'posts' => 'posts#create'
+  
+  get 'top' => 'root#top'
 end
